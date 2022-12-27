c53296a42d90ce450ff4ddf28a2bab2e73d627c7
skkim <96033556+bss-skkim@users.noreply.github.com>
2022-12-27 11:25:08 +0900

    add project schemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -2,5 +2,16 @@ import blockContent from './blockContent'
 import category from './category'
 import post from './post'
 import author from './author'
+import project from './project'
+import projectType from './projectType'
+import projectSummaries from './projectSummaries'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [
+  post,
+  author,
+  category,
+  blockContent,
+  project,
+  projectType,
+  projectSummaries,
+]
---
+++ schemas/project.ts
@@ -0,0 +1,93 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+    }),
+    defineField({
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'projectType',
+      title: 'Project Type',
+      type: 'reference',
+      to: {type: 'projectType'},
+    }),
+    defineField({
+      name: 'workedAt',
+      title: 'Worked at',
+      type: 'string',
+    }),
+    defineField({
+      name: 'workStartedAt',
+      title: 'Work Started at',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'workEndedAt',
+      title: 'Work Ended at',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'author',
+      title: 'Author',
+      type: 'reference',
+      to: {type: 'author'},
+    }),
+    defineField({
+      name: 'summaries',
+      title: 'Summaries',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'projectSummaries'}}],
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    }),
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      description: 'description',
+      author: 'author.name',
+      projectType: 'projectType.title',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      const {author} = selection
+      return {...selection, subtitle: author && `by ${author}`}
+    },
+  },
+})
